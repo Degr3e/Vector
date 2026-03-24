a75d18f4b31571ea43ad25d20c48e8ea418fece9
Yugo <95368001+Degr3e@users.noreply.github.com>
2026-03-24 10:13:36 +0300

    implimentation more methods and tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "top-it-vector.hpp"
 #include <iostream>
-#include <iomanip>
 #include <stdexcept>
 
 bool testEmptyVector()
 {
-  std::cout << __func__ << std::endl;
   using namespace topit;
   Vector< int > v;
   return v.isEmpty();
@@ -29,7 +27,39 @@ bool testElementsOutOfBoundAcces()
 {
   topit::Vector< int > v;
   try{
-    int & val = v.at(0);
+    v.at(0);
+    return false;
+  }
+  catch (const std::out_of_range &)
+  {
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool TestElementConstAccess()
+{
+  topit::Vector< int > v;
+  const topit::Vector< int > & rv = v;
+  v.pushBack(1);
+  try{
+    const int & val = rv.at(0);
+    return val == 1;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool testElementsOutOfBoundConstAcces()
+{
+  const topit::Vector< int > v;
+  try{
+    v.at(0);
     return false;
   }
   catch (const std::out_of_range &)
@@ -50,14 +80,16 @@ int main()
     { "Empty vector", testEmptyVector},
     { "Indbound access", TestElementAccess},
     { "Out of bound access", testElementsOutOfBoundAcces},
+    { "Inbound const access", TestElementConstAccess},
+    { "Out of bound access", testElementsOutOfBoundConstAcces},
   };
   const size_t count = sizeof(tests) / sizeof(test_t);
   std::cout << std::boolalpha;
   bool pass = true;
   for (size_t i = 0; i < count; ++i)
   {
-    bool res = tests[i].second;
-    std::cout << tests[i].first << ": " << tests[i].second() << "\n";
+    bool res = tests[i].second();
+    std::cout << tests[i].first << ": " << res << "\n";
     pass = pass && res;
   }
   std::cout << "RESULT: " << pass << "\n";
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -27,6 +27,7 @@ namespace topit
     void pushBack(const T&);
     void popBack();
     void insert(size_t, const T&);
+    void erase(size_t);
     void earse(size_t);
   private:
     T* data_;
@@ -37,11 +38,17 @@ namespace topit
 template< class T >
 T& topit::Vector< T >::at(size_t id)
 {
+  return const_cast< T& >(static_cast< const Vector< T >* >(this)->at(id));
+}
+
+template< class T >
+const T& topit::Vector< T >::at(size_t id) const
+{
   if(id < getSize())
   {
     return data_[id];
   }
-  throw std::range_error("bad id");
+  throw std::out_of_range("Vector::at bad id");
 }
 
 template< class T >
@@ -61,6 +68,16 @@ void topit::Vector< T >::pushBack(const T& value)
   }
   data_[size_] = value;
   ++size_;
+}
+template< class T >
+T& topit::Vector< T >::operator[](size_t id) noexcept
+{
+  return data_[id];
+}
+template< class T >
+const T& topit::Vector< T >::operator[](size_t id) const noexcept
+{
+  return data_[id];
 }
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
@@ -99,4 +116,118 @@ topit::Vector< T >::Vector():
   size_(0),
   capacity_(0)
 {}
+template< class T >
+topit::Vector< T >::Vector(const Vector& other):
+  data_(nullptr),
+  size_(other.size_),
+  capacity_(other.capacity_)
+{
+  if (capacity_ != 0)
+  {
+    data_ = new T[capacity_];
+    for (size_t i = 0; i < size_; ++i)
+    {
+      data_[i] = other.data_[i];
+    }
+  }
+}
+template< class T >
+topit::Vector< T >::Vector(Vector&& other):
+  data_(other.data_),
+  size_(other.size_),
+  capacity_(other.capacity_)
+{
+  other.data_ = nullptr;
+  other.size_ = 0;
+  other.capacity_ = 0;
+}
+template< class T >
+topit::Vector< T >& topit::Vector< T >::operator=(const Vector& other)
+{
+  if (this == &other)
+  {
+    return *this;
+  }
+  T* newData = nullptr;
+  if (other.capacity_ != 0)
+  {
+    newData = new T[other.capacity_];
+    for (size_t i = 0; i < other.size_; ++i)
+    {
+      newData[i] = other.data_[i];
+    }
+  }
+  delete [] data_;
+  data_ = newData;
+  size_ = other.size_;
+  capacity_ = other.capacity_;
+  return *this;
+}
+template< class T >
+topit::Vector< T >& topit::Vector< T >::operator=(Vector&& other)
+{
+  if (this == &other)
+  {
+    return *this;
+  }
+  delete [] data_;
+  data_ = other.data_;
+  size_ = other.size_;
+  capacity_ = other.capacity_;
+  other.data_ = nullptr;
+  other.size_ = 0;
+  other.capacity_ = 0;
+  return *this;
+}
+template< class T >
+void topit::Vector< T >::insert(size_t id, const T& value)
+{
+  if (id > size_)
+  {
+    throw std::out_of_range("Vector::insert bad id");
+  }
+  if (size_ == capacity_)
+  {
+    size_t newCapacity = capacity_ == 0 ? 1 : capacity_ * 2;
+    T* newData = new T[newCapacity];
+    for (size_t i = 0; i < id; ++i)
+    {
+      newData[i] = std::move(data_[i]);
+    }
+    newData[id] = value;
+    for (size_t i = id; i < size_; ++i)
+    {
+      newData[i + 1] = std::move(data_[i]);
+    }
+    delete [] data_;
+    data_ = newData;
+    capacity_ = newCapacity;
+    ++size_;
+    return;
+  }
+  for (size_t i = size_; i > id; --i)
+  {
+    data_[i] = std::move(data_[i - 1]);
+  }
+  data_[id] = value;
+  ++size_;
+}
+template< class T >
+void topit::Vector< T >::erase(size_t id)
+{
+  if (id >= size_)
+  {
+    throw std::out_of_range("Vector::erase bad id");
+  }
+  for (size_t i = id; i + 1 < size_; ++i)
+  {
+    data_[i] = std::move(data_[i + 1]);
+  }
+  --size_;
+}
+template< class T >
+void topit::Vector< T >::earse(size_t id)
+{
+  erase(id);
+}
 #endif
